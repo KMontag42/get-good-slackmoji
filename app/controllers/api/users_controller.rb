d94165a99902b91f57cf64ab9cab11dcd5a55d9e
kyle <thekylemontag@gmail.com>
2016-10-18 14:39:06 -0400

    properly use the email if slack username isn't set

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,9 +3,13 @@ module Api
     before_action :authenticate_user!
 
     def import
+      username = current_user.slack_username.blank? ?
+          current_user.email :
+          current_user.slack_username
+
       # fallback to account email if slack_username is nil
       UsersImportJob.perform_later(current_user.slack_domain,
-                                   current_user.slack_username || current_user.email,
+                                   username,
                                    params[:password],
                                    params[:emoji].permit!.to_h)
       render json: {}
