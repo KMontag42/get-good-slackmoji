15bef19af67b45897bf997049cd2c75b5ea7622e
kyle <thekylemontag@gmail.com>
2016-10-18 14:35:53 -0400

    add a puma worker to run active job

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url,        'git@github.com:KMontag42/memeporter.git'
 set :application,     'memeporter'
 set :user,            'kyle'
 set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :puma_workers,    1
 
 # Don't change these unless you know what you're doing
 set :pty,             true
