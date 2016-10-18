f8fb6b518ab6a886695e26008ae84bc6fd01d596
kyle <thekylemontag@gmail.com>
2016-10-18 12:12:36 -0400

    use slack token from ENV for emoji list

--- app/models/emoji.rb
+++ app/models/emoji.rb
@@ -5,6 +5,6 @@ class Emoji
   base_uri 'https://slack.com/api'
 
   def self.get_emoji
-    get('/emoji.list?token=xoxp-15339600372-15344814513-82242859588-0094603eba9a37cc80f218b8c16e1243&pretty=1')
+    get('/emoji.list?pretty=1&token='+ENV['SLACK_TOKEN'])
   end
 end
