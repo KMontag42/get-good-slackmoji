9857bbb47e96e66ea2dedc5b252d77973016380f
kyle <thekylemontag@gmail.com>
2016-10-18 15:28:20 -0400

    bundle exec puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,7 +54,7 @@ namespace :puma do
         end
         within current_path do
           with rack_env: fetch(:puma_env) do
-            execute 'xvfb-run', "puma -C #{fetch(:puma_conf)} --daemon"
+            execute 'xvfb-run', "bundle exec puma -C #{fetch(:puma_conf)} --daemon"
           end
         end
       end
